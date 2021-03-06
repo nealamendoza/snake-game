e04fcd4de0bb93ae79d43284428093111965856b
nealamendoza <mendo151@cougars.csusm.edu>
2021-03-06 15:58:10 -0800

    script.js

---
+++ scripts/script.js
@@ -0,0 +1,136 @@
+console.log("Hello");
+
+let canvas = document.getElementsByTagName("canvas")[0];
+let ctx = canvas.getContext('2d');
+
+let height = canvas.height;
+let width = canvas.width;
+
+const SIZE = 10;
+
+let dx = 0;
+let dy = SIZE;
+
+function drawRect(x,y) {
+    ctx.fillRect(x, y, SIZE, SIZE);
+
+}
+
+let food = {
+    x: 0,
+    y: 0
+
+};
+console.log(food);
+
+function addFood() {
+    let xVal = Math.floor(Math.random() * width / SIZE) * SIZE;
+    let yVal = Math.floor(Math.random() * height / SIZE) * SIZE;
+
+    food = {
+        x: xVal,
+        y:yVal
+    }
+}
+
+addFood();
+console.log(food);
+drawRect(food.x, food.y);
+
+let snake = [];
+function addSnake(xVal, yVal) {
+    snake.push({
+        x:xVal,
+        y:yVal
+    })
+}
+
+console.log(snake)
+addSnake(0,0)
+addSnake(SIZE,0)
+addSnake(SIZE*2, 0)
+console.log(snake)
+
+
+for(let i = 0; i < snake.length; i++) {
+    drawRect(snake[i].x, snake[i].y);
+}
+
+function drawSnake() {
+    for(let i = 0; i < snake.length; i++) {
+        drawRect(snake[i].x, snake[i].y);
+    }
+}
+
+function create(){
+    ctx.clearRect(0, 0, width, height)
+
+    snake.shift();
+
+    if(foodCollide()) {
+        addHead();
+        addFood();
+    }
+
+    addHead();
+
+    drawSnake();
+    drawRect(food.x, food.y);
+}
+create()
+
+
+
+function addHead() {
+    let x = snake[snake.length - 1].x + dx;
+    let y = snake[snake.length - 1].y + dy;
+
+    if(x > width) {
+        x = 0;
+    }
+    else if(y > height){
+        y = 0;
+    }
+    else if(x < 0) {
+        x = width;
+    }
+    else if(y < 0){
+        y = height;
+    }
+
+    addSnake(x, y);
+}
+
+let timer = setInterval(create, 100);
+
+window.addEventListener("keydown", keys)
+
+function keys(e) {
+    if (e.keyCode == 38) {
+        console.log("up");
+        dx = 0;
+        dy = -SIZE;
+    } else if (e.keyCode == 39) {
+        console.log("right");
+        dx = SIZE;
+        dy = 0;
+    } else if (e.keyCode == 40) {
+        console.log("down");
+        dx = 0;
+        dy = SIZE;
+    } else if (e.keyCode == 37) {
+        console.log("left");
+        dx = -SIZE;
+        dy = 0;
+    }
+}
+
+function foodCollide() {
+    let head = snake[snake.length - 1];
+
+    if(head.x == food.x && head.y == food.y) {
+        return true;
+    }
+
+    return false;
+}
